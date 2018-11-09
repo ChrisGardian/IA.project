fc85215ee11f6f9662084acf3ced32097bb357f2
ChrisGardian <44732253+ChrisGardian@users.noreply.github.com>
2018-11-09 12:42:49 +0100

    Update intelligence_artificielle.py

--- intelligence_artificielle.py
+++ intelligence_artificielle.py
@@ -1,5 +1,5 @@
-#3 variables inutiles
-
+#3 u 
+import loging
 
 
 
@@ -9,34 +9,29 @@ name_chris=['christopher','chris','Chris','Christopher']
 name_freresoeur=["Estelle","estelle","mathieu","Mathieu"]
 answer_stop=["non", "Non", "no", "No", "n", "N",'exit','Exit']
 answer_yes=['Ouais','ouais','Oui','oui','Yes','yes','Yep','yep','Y','y','oui merci','Oui merci']
-answer_cava=['Ca va?','ca va?','Ca va','ca va','ca va ?','Ca va ?',"Est ce que ca va?","est ce que ca va?","Est-ce que ca va?","est-ce que ca va?"]
 
 
 
 
 def question():    
-    print('')
-    if True: 
-        q=raw_input('Avez vous une question?')
-    else:
-        q='Qui est le meilleur?'
+    q=raw_input("Avvez-vous une question?")
     while (q not in answer_stop):
-        if q=='Qui est le meilleur?' or q=='qui est le meilleur?':
+       
+       if ("Qui" or "qui") and ("meilleur") in q:
             if a==True:
                 print("C'est vous maitre bien sur")
             else:
                 print("C'est le maitre Christopher bien sur.")
-        
-        elif q in answer_cava:
-            print('Tres bien. Merci beaucoup de demander')
             
-        elif q in answer_yes:
+       
+       elif q in answer_yes:
             if a==False:
                 print("Ben pose la qu'est ce que t'attend.")
             else:                
                 print("J'attends la question avec impatience")
                 
-        elif q in answer_cava:
+       
+       elif ("Ca" or "ca") and ("va") in q:
             b=raw_input('Bien et est ce que ca va?')
             if a==False:            
                 if b in answer_yes:
@@ -52,12 +47,15 @@ def question():
                     raw_input("Ah bon! Qu'est ce qui ne va pas?")
                     c=raw_input("Oui je vois. Est ce que je peut faire quelque chose?")
                     if c in answer_yes:
-                            raw_input=("Tres bien qu'est ce que je peut faire?")
-                            
-                else:
-                    print("Reponds correctement aux question s'il te plait")  
-            
-        else:
+                            raw_input("Tres bien qu'est ce que je peut faire?")
+                            print("Je ferais de mon mieux")
+                    else:
+                        print("Reponds correctement aux question s'il te plait")  
+       
+       
+       #elif: 
+        
+       else:
             print('Je ne suis pas encore programmee pour repondre a une telle question')
         q=raw_input('Avez vous une autre question?')
     print("Au revoir! J'espere que vous passez une bonne journee.")
@@ -95,7 +93,6 @@ def rien():
 
 
 
-question()
 print('')
 print("Pour tout le reste ou tu seras avec moi je te prie d'ecrire clairement avec ponctuation et sans espace inutile" )
 name=raw_input('Bonjour!Quel est ton nom ?  ')
